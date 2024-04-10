67bbd9284aa6a60607dfff2777f88a3ca9eedff5
Subhiksha <subhiksha102@gmail.com>
2024-04-10 11:53:09 +0530

    Hackathon

---
+++ src/test/java/testRunner/testRun.java
@@ -0,0 +1,25 @@
+package testRunner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+				features= {".//Feature/"},
+				//features= {".//Feature//F1_top5Doctors.feature",".//Feature//F2_surgeryPage.feature",
+				//		".//Feature//F3_forCorporate.feature"},
+				glue="stepDefinitions",
+				plugin= {"pretty", "html:reports/myreport.html", 
+						  "rerun:target/rerun.txt",
+						  "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+				tags="@sanity" 
+				//tags="@regression"
+				//tags="@sanity and @regression" 
+				//tags="@sanity and not @regression"
+				//tags="@sanity or @regression"
+				)
+public class testRun {
+
+}
